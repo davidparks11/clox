a06adac593047c610aad1895029411b071943549
David Parks <davidparks11@gmail.com>
2024-03-02 22:26:41 -0600

    pg 258

--- src/main.c
+++ src/main.c
@@ -5,11 +5,12 @@
 int main(int argc, const char* argv[]) {
 	Chunk chunk;
 	initChunk(&chunk);
+	
 	int constant = addConstant(&chunk, 21.3);
 	writeChunk(&chunk, OP_CONSTANT);
 	writeChunk(&chunk, constant);
+	
 	writeChunk(&chunk, OP_RETURN);
-
 	disassembleChunk(&chunk, "test chunk");
 	freeChunk(&chunk);
 	return 0;
