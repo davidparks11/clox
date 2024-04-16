66bcb559a0286a7a45b22fde6b2680f15ca265f9
David Parks <davidparks11@gmail.com>
2024-04-16 01:14:51 -0400

    15.3.1 complete negation + format files

--- src/main.c
+++ src/main.c
@@ -1,18 +1,22 @@
-#include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
-int main(int argc, const char* argv[]) {
-	Chunk chunk;
-	initChunk(&chunk);
-	
-	int constant = addConstant(&chunk, 21.3);
-	writeChunk(&chunk, OP_CONSTANT, 123);
-	writeChunk(&chunk, constant, 123);
-	
-	writeChunk(&chunk, OP_RETURN, 123);
-	disassembleChunk(&chunk, "test chunk");
-	freeChunk(&chunk);
-	return 0;
+int main(int argc, const char *argv[]) {
+  initVM();
+
+  Chunk chunk;
+  initChunk(&chunk);
+
+  int constant = addConstant(&chunk, 21.3);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
+  writeChunk(&chunk, OP_NEGATE, 123);
+
+  writeChunk(&chunk, OP_RETURN, 123);
+  disassembleChunk(&chunk, "test chunk");
+  interpret(&chunk);
+  freeVM();
+  freeChunk(&chunk);
+  return 0;
 }
-
